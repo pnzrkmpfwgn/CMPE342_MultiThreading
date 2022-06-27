7b540c19522aec680c01d7e1f2d47c32581bd8a6
Ayberk Yiğit <kurtknispel9@gmail.com>
2022-06-27 20:24:21 +0300

    Example Completed

---
+++ MyThread.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp2
+{
+    public class MyThread
+    {
+        public static void Thread1()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(MyThread.Thread1));
+            Thread t2 = new Thread(new ThreadStart(MyThread.Thread1));
+
+            t1.Start();
+            t2.Start();
+        }
+    }
+}
