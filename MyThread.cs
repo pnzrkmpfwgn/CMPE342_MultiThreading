9fd6164e741a80699207f2cfa7cdaf206d21dde5
Ayberk Yiğit <kurtknispel9@gmail.com>
2022-06-27 20:28:48 +0300

    Example Completed

--- MyThread.cs
+++ MyThread.cs
@@ -7,20 +7,28 @@ namespace ConsoleApp2
     {
         public static void Thread1()
         {
-            for(int i = 0; i < 10; i++)
+            Thread t = Thread.CurrentThread;
+            for(int i = 0; i < 5; i++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(t.Name + " " + i);
                 Thread.Sleep(200);
             }
         }
 
         static void Main(string[] args)
         {
+
             Thread t1 = new Thread(new ThreadStart(MyThread.Thread1));
             Thread t2 = new Thread(new ThreadStart(MyThread.Thread1));
+            Thread t3 = new Thread(new ThreadStart(MyThread.Thread1));
+
+            t1.Name = "T1";
+            t2.Name = "T2";
+            t3.Name = "T3";
 
             t1.Start();
             t2.Start();
+            t3.Start();
         }
     }
 }
